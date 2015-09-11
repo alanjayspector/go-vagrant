303b00db7616f03f3e9305b372659a40dacd167b
Alan Spector <github@aj5.org>
2015-09-10 23:30:45 -0700

    silence installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get -y install curl
-sudo apt-get -y install git
+sudo apt-get update > /dev/null
+sudo apt-get -y install curl > /dev/null
+sudo apt-get -y install git > /dev/null
 curl -s https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz > go1.5.1.linux-amd64.tar.gz
 sudo tar xf go1.5.1.linux-amd64.tar.gz -C /usr/local
 rm go1.5.1.linux-amd64.tar.gz
